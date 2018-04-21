83f446c34132f43194b5dca9aa5ff71d99b3b55d
ThomasVandenhede <thomasvandenhede@hotmail.com>
2018-04-21 23:17:31 +0200

    Add new Vector class

---
+++ js/vector.js
@@ -0,0 +1,36 @@
+function Vector(x, y) {
+  this.x = x || 0;
+  this.y = y || 0;
+}
+
+Object.defineProperties(Vector.prototype, {
+  'direction': {
+    get: function() {
+      return Math.atan2(this.y, this.x);
+    },
+    set: function(direction) {
+      var norm = this.norm;
+      this.x = norm * Math.cos(direction);
+      this.y = norm * Math.sin(direction);
+    }
+  },
+  'norm': {
+    get: function() {
+      return Math.sqrt(this.x ** 2 + this.y ** 2);
+    },
+    set: function(norm) {
+      var ratio = this.x / this.y;
+      // this.x = norm * Math.cos(this.direction);
+      // this.y = norm * Math.sin(this.direction);
+      this.x = norm * ratio / Math.sqrt(1 + ratio ** 2);
+      this.y = norm / Math.sqrt(1 + ratio ** 2);
+    }
+  }
+});
+
+var v = new Vector(3, 4);
+console.log(v.x, v.y, v.norm);
+v.x = 15;
+v.y = 10;
+v.norm = 5;
+console.log(v.x, v.y, v.direction);
