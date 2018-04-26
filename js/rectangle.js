4b0caabb7017ae2bc102dddf0b3eddbf52440029
ThomasVandenhede <thomasvandenhede@hotmail.com>
2018-04-26 16:22:35 +0200

    Add set, move and moveTo methods to Rectangle class

--- js/rectangle.js
+++ js/rectangle.js
@@ -1,6 +1,31 @@
-function Rectangle(x, y, w, h) {
-  this.x = x;
-  this.y = y;
-  this.w = w;
-  this.h = h;
+function Rectangle(left, top, width, height) {
+  this.left = left;
+  this.top = top;
+  this.width = width;
+  this.height = height;
+  this.right = left + width;
+  this.bottom = top + height;
 }
+
+Rectangle.prototype.set = function(left, top, width, height) {
+  this.left = left;
+  this.top = top;
+  this.width = width || this.width;
+  this.height = height || this.height;
+  this.right = this.left + this.width;
+  this.bottom = this.top + this.height;
+};
+
+Rectangle.prototype.move = function(dx, dy) {
+  this.left += dx;
+  this.top += dy;
+  this.right = this.left + this.width;
+  this.bottom = this.top + this.height;
+};
+
+Rectangle.prototype.moveTo = function(left, top) {
+  this.left = left;
+  this.top = top;
+  this.right = this.left + this.width;
+  this.bottom = this.top + this.height;
+};
