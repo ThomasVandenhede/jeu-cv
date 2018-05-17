21f08b4146edfb112a293ee8aaadfcc9debffaab
ThomasVandenhede <thomasvandenhede@hotmail.com>
2018-05-17 10:25:37 +0200

    Clean up Sound class

--- js/sound.js
+++ js/sound.js
@@ -1,12 +1,5 @@
 function Sound(src) {
   this.sound = new Audio(src);
-  // this.sound = document.createElement("audio");
-  // this.sound.src = src;
-  // this.sound.setAttribute("preload", "auto");
-  // this.sound.setAttribute("controls", "none");
-  // this.sound.style.display = "none";
-  // document.body.appendChild(this.sound);
-  console.log('' + this.sound);
 }
 
 Sound.prototype.play = function() {
@@ -24,9 +17,9 @@ Sound.prototype.stop = function() {
 
 Sound.prototype.rewind = function() {
   this.sound.currentTime = 0;
-}
+};
 
 Sound.prototype.replay = function() {
   this.rewind();
   this.play();
-}
+};
