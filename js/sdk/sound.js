61858ba3889dd698b745158524af8bd09e9309c5
ThomasVandenhede <thomasvandenhede@hotmail.com>
2018-05-26 19:41:07 +0200

    loop and isMusic flags

--- js/sdk/sound.js
+++ js/sdk/sound.js
@@ -1,27 +1,33 @@
-function Sound(src, volume, loop) {
+function Sound(src, volume, options) {
+  if (options) {
+    this.sound.loop = options.loop || false;
+    this.sound.isMusic = options.isMusic || false;
+  }
   this.sound = new Audio(src);
-  this.sound.volume = (volume !== undefined) ? volume : 1;
-  this.sound.loop = loop || false;
+  this.sound.volume = volume !== undefined ? volume : 1;
+  this.isPlaying = false;
 }
 
-Sound.prototype.play = function () {
+Sound.prototype.play = function() {
   this.sound.play();
+  this.isPlaying = true;
 };
 
-Sound.prototype.pause = function () {
+Sound.prototype.pause = function() {
   this.sound.pause();
+  this.isPlaying = true;
 };
 
-Sound.prototype.stop = function () {
+Sound.prototype.stop = function() {
   this.pause();
   this.rewind();
 };
 
-Sound.prototype.rewind = function () {
+Sound.prototype.rewind = function() {
   this.sound.currentTime = 0;
 };
 
-Sound.prototype.replay = function () {
+Sound.prototype.replay = function() {
   this.rewind();
   this.play();
 };
